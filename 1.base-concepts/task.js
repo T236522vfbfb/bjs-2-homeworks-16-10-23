0806b83428c43a2e2abaa1f364aa8258f47c92f5
Name Surname <email@email.com>
2023-10-16 21:08:45 +0300

    «Основные понятия» 1

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,7 +1,15 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let dicr = b**-4*a*c;
+  if (dicr < 0) {
+    arr = [];
+  } else if (dicr == 0) {
+    arr[0] = -b/(2*a);
+  } else {
+    arr[0] = (-b + Math.sqrt(d) )/(2*a);
+    arr[1] = (-b - Math.sqrt(d) )/(2*a);
+  }
   return arr;
 }
 
