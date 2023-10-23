b103d68c0784392f302275d34c5c2299193ecad0
Name Surname <email@email.com>
2023-10-23 20:03:17 +0300

    toFixed

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,15 +1,15 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  let dicr = b**2-4*a*c;
-  if (dicr < 0) {
-    arr = [];
-  } else if (dicr == 0) {
+  let discr = b**2-4*a*c;
+
+  if (discr == 0) {
     arr[0] = -b/(2*a);
-  } else {
-    arr[0] = (-b + Math.sqrt(d) )/(2*a);
-    arr[1] = (-b - Math.sqrt(d) )/(2*a);
+  } else if (discr > 0) {
+    arr[0] = (-b + Math.sqrt(discr) )/(2*a);
+    arr[1] = (-b - Math.sqrt(discr) )/(2*a);
   }
+
   return arr;
 }
 
@@ -17,6 +17,6 @@ function calculateTotalMortgage(percent, contribution, amount, countMonths) {
   let P = percent/100/12;
   let S = amount-contribution;
   let month_pay = S * (P + (P / (((1 + P)**countMonths) - 1)));
-  let result = S + Math.round(month_pay*100)/100;
-  return result;
+  let result = month_pay * countMonths;
+  return result.toFixed(2);
 }
