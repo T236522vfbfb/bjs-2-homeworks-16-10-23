56ebd545c1f14aee969df413ee4e7c508144d6fc
Name Surname <email@email.com>
2023-10-30 20:08:33 +0300

    Правка 30,10,2023

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,29 +1,42 @@
 function Student(name, gender, age) {
-    this.name = name;
-    this.gender = gender;
-    this.age = age;
-  }
-  
-  Student.prototype.setSubject = function (subjectName) {
-    this.subject = subjectName;
-  }
-  
-  Student.prototype.addMark = function (mark) {
-    (this.marks === undefined) ? this.marks = [mark] : this.marks.push(mark);
-  }
-  
-  Student.prototype.addMarks = function (...marks) {
-    (this.marks === undefined) ? this.marks = [...marks] : this.marks.push(...marks);
-  }
-  
-  Student.prototype.getAverage = function () {
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+}
+
+Student.prototype.setSubject = function (subjectName) {
+  this.subject = subjectName;
+}
+
+Student.prototype.addMark = function (mark) {
+  (this.marks === undefined) ? this.marks = [mark] : this.marks.push(mark);
+}
+
+Student.prototype.addMarks = function (...marks) {
+  (this.marks === undefined) ? this.marks = [...marks] : this.marks.push(...marks);
+}
+
+Student.prototype.getAverage = function () {
+  if (this.marks === undefined) {
+    return 0;
+  } else {
     let sum = 0;
     this.marks.forEach(item => sum += item);
     return sum / this.marks.length;
   }
-  
-  Student.prototype.exclude = function (reason) {
-    delete this.subject;
-    delete this.marks;
-    this.excluded = reason;
-  }
+}
+
+Student.prototype.exclude = function (reason) {
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
+}
+
+let student1 = new Student("Василиса", "женский", 19);
+student1.setSubject("Algebra");
+student1.addMarks(4, 5, 4, 5);
+
+
+let student2 = new Student("Артём", "мужской", 25);
+student2.setSubject("Geometry");
+student2.exclude('плохая учёба');
