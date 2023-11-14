b0500fd03616601238c4ff73c01213986a9f7234
Name Surname <email@email.com>
2023-11-14 20:22:11 +0300

    Правка 14,11/33

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -21,7 +21,7 @@ class Triangle {
         this.b = b;
         this.c = c;
     
-        if (a + b < c || a + c < b || b + c < a) {
+        if (a + b <= c || a + c < b || b + c < a) {
            throw new Error ('Треугольник с такими сторонами не существует');
         }       
     }
